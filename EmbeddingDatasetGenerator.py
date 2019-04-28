57d03638a3c61f056ef30c5c013d7a102d2558e7
nikhil sulegaon <nikhilsulegaon@gmail.com>
2019-04-28 03:04:14 -0600

    Creating training triplets for word embeddings

--- EmbeddingDatasetGenerator.py
+++ EmbeddingDatasetGenerator.py
@@ -13,20 +13,49 @@ class EmbeddingDatasetGenerator(object):
     def __init__(self, raw_dataset):
         self.raw_dataset = raw_dataset
 
-    def __random_neg_document(self, doc_id):
-        in_citations = self.raw_dataset.in_citations(doc_id)
-        exclude_list = [doc_id] + in_citations
+    def __random_neg_document(self, doc_id, n=1):
+        out_citations = self.raw_dataset.oout_citations(doc_id)
+        exclude_list = [doc_id] + out_citations
 
         def random_id(excluding, range_lim, retry_count):
             if retry_count > 100:
                 raise Exception('Cannot generate Random Negative Sample')
             rand_id = randint(1, range_lim)
+            # Using recursion to optimize for memory while "re-rolling"
             return random_id(exclude_list, range_lim, retry_count + 1) if rand_id in excluding else rand_id
 
         lim = self.raw_dataset.count(DBConfig.dataset_collection())
-        neg_doc_id = random_id(exclude_list, lim, 0)
-        return self.raw_dataset.find_one_by_doc_id(neg_doc_id)
+        return [self.raw_dataset.find_one_by_doc_id(random_id(exclude_list, lim, 0)) for _ in range(n)]
 
-    def negative_document(self, doc_id, technique):
+    def __nested_citation_neg_document(self, doc_id, n=None):
+        out_citations = self.raw_dataset.out_citation_ids(doc_id)
+        nested_out_citations = []
+        for cite_id in out_citations:
+            nested_out_citations += self.raw_dataset.out_citation_ids(cite_id)
+
+        nested_cite_neg_docs = []
+        ids = range(len(nested_out_citations)) if not n else [randint(0, len(nested_out_citations)) for _ in n]
+        for index in ids:
+            nested_cite_neg_docs.append(self.raw_dataset.find_one_by_doc_id(nested_out_citations[index]))
+        return nested_cite_neg_docs
+
+    def negative_document(self, doc_id, technique, n):
         if technique == Technique.RANDOM:
-            return self.__random_neg_document(doc_id)
+            return self.__random_neg_document(doc_id, n)
+
+        else:
+            return self.__nested_citation_neg_document(doc_id, n)
+
+    def positive_document(self, doc_id):
+        return self.raw_dataset.out_citation_docs(doc_id)
+
+    def generate_training_triplets(self, doc_id):
+        d_q = self.raw_dataset.find_one_by_doc_id(doc_id)
+        d_pos = self.positive_document(doc_id)
+        n_rand_neg = len(d_pos) // 2
+        n_nested_neg = len(d_pos) - n_rand_neg
+        d_neg = self.negative_document(doc_id, Technique.RANDOM, n_rand_neg) + \
+                self.negative_document(doc_id, Technique.NESTED_CITE, n_nested_neg)
+
+        # No cross-product
+        return zip([d_q] * len(d_pos), d_pos, d_neg)
--- RawDataset.py
+++ RawDataset.py
@@ -2,10 +2,6 @@ from DBConfig import DBConfig
 from DBConfig import MongoClient
 
 
-# inCitations  List of S2 paper IDs which cited this paper.
-# outCitations  List of S2 paper IDs which this paper cited.
-
-
 class RawDataset(object):
     def __init__(self, database=MongoClient.mongo_database()):
         self.database = database
@@ -13,17 +9,30 @@ class RawDataset(object):
     def __collection(self, collection_name):
         return self.database[collection_name]
 
-    def in_citations(self, doc_id):
+    # inCitations - List of paper IDs which cited this(doc_id) paper.
+    def in_citation_ids(self, doc_id):
         if DBConfig.in_citation_collection() in self.database.list_collection_names():
             return self.__collection(DBConfig.in_citation_collection()).find_one({'id': str(doc_id)})['citedBy']
         return map(lambda rec: rec['id'],
                    self.__collection(DBConfig.dataset_collection()).find({'out_citations': str(doc_id)}))
 
-    def out_citations(self, doc_id):
+    # outCitations - List of paper IDs which this paper(doc_id) cited.
+    def out_citation_ids(self, doc_id):
         return self.__collection(DBConfig.dataset_collection()).find_one({'id': str(doc_id)})['out_citations']
+
+    def in_citation_docs(self, doc_id):
+        in_citation_ids = self.in_citation_ids(doc_id)
+        return self.find_by_doc_ids(in_citation_ids)
+
+    def out_citation_docs(self, doc_id):
+        out_citation_ids = self.out_citation_ids(doc_id)
+        return self.find_by_doc_ids(out_citation_ids)
 
     def find_one_by_doc_id(self, doc_id):
         return self.__collection(DBConfig.dataset_collection()).find_one({'id': str(doc_id)})
+
+    def find_by_doc_ids(self, doc_ids):
+        return [self.find_one_by_doc_id(doc_id) for doc_id in doc_ids]
 
     def count(self, collection_name):
         if collection_name in self.database.list_collection_names():
