e152d64f4df2b04f8d5cee26cdb1527eb59ae4b5
nikhil sulegaon <nikhilsulegaon@gmail.com>
2019-04-28 04:22:49 -0600

    Timing data generation operation

--- models/word_embeddings/embedding_model.py
+++ models/word_embeddings/embedding_model.py
@@ -1,10 +1,12 @@
+import time
 from dataset_generator.RawDataset import RawDataset
 from dataset_generator.word_embeddings.EmbeddingDatasetGenerator import EmbeddingDatasetGenerator
 
 batch_size = 32
 
+start = time.time()
 dataset_generator = EmbeddingDatasetGenerator(RawDataset())
 
 output = dataset_generator.generate_training_data_for_epoch(32)
-
-print(len(output))
+end = time.time()
+print(end - start)
